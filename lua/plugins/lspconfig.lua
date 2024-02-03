7ce6aeebd0fa73580428761da93e6a09cc0a686a
Joao AAFN <joao.aafn@gmail.com>
2024-02-03 17:49:22 -0300

    lsp repair

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -20,7 +20,7 @@ return {
         lazy = false,
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "tsserver" },
+                auto_install = true,
             })
         end,
         opts = {
